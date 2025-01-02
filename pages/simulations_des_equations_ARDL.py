538afd905eaf6df9dd9c1c9d6cee8ec85d57e7ba
cezangue <cezangue@gmail.com>
2025-01-02 18:55:57 +0100

    Update simulations_des_equations_ARDL.py

--- pages/simulations_des_equations_ARDL.py
+++ pages/simulations_des_equations_ARDL.py
@@ -29,45 +29,64 @@ def model_equations(data):
     X1['G_lag'] = data['G'].shift(1)
     X1['X_lag'] = data['X'].shift(1)
     X1['M_lag'] = data['M'].shift(1)
-    X1 = sm.add_constant(X1.dropna())
-    model1 = sm.OLS(Y1.dropna(), X1).fit()
 
+    # Créer un DataFrame avec les valeurs non manquantes
+    combined_data = pd.concat([Y1, X1], axis=1).dropna()
+    Y1_clean = combined_data['Pib']
+    X1_clean = sm.add_constant(combined_data.drop(columns='Pib'))
+
+    model1 = sm.OLS(Y1_clean, X1_clean).fit()
+
+    # Répéter le même processus pour les autres équations
     # Équation 2: DCF
     Y2 = data['DCF']
     X2 = data[['Taux_interet', 'Pib']]
     X2['DCF_lag'] = data['DCF'].shift(1)
-    X2['Pib_lag'] = data['Pib'].shift(1)
-    X2 = sm.add_constant(X2.dropna())
-    model2 = sm.OLS(Y2.dropna(), X2).fit()
+    combined_data = pd.concat([Y2, X2], axis=1).dropna()
+    Y2_clean = combined_data['DCF']
+    X2_clean = sm.add_constant(combined_data.drop(columns='DCF'))
+
+    model2 = sm.OLS(Y2_clean, X2_clean).fit()
 
     # Équation 3: FBCF
     Y3 = data['FBCF']
     X3 = data[['Taux_interet', 'Pib']]
     X3['FBCF_lag'] = data['FBCF'].shift(1)
-    X3 = sm.add_constant(X3.dropna())
-    model3 = sm.OLS(Y3.dropna(), X3).fit()
+    combined_data = pd.concat([Y3, X3], axis=1).dropna()
+    Y3_clean = combined_data['FBCF']
+    X3_clean = sm.add_constant(combined_data.drop(columns='FBCF'))
+
+    model3 = sm.OLS(Y3_clean, X3_clean).fit()
 
     # Équation 4: MM
     Y4 = data['MM']
     X4 = data[['Pib', 'Taux_interet', 'Infflation']]
     X4['MM_lag'] = data['MM'].shift(1)
-    X4 = sm.add_constant(X4.dropna())
-    model4 = sm.OLS(Y4.dropna(), X4).fit()
+    combined_data = pd.concat([Y4, X4], axis=1).dropna()
+    Y4_clean = combined_data['MM']
+    X4_clean = sm.add_constant(combined_data.drop(columns='MM'))
+
+    model4 = sm.OLS(Y4_clean, X4_clean).fit()
 
     # Équation 5: TC
     Y5 = data['TC']
     X5 = data[['Pib', 'Pibmond']]
     X5['TC_lag'] = data['TC'].shift(1)
-    X5 = sm.add_constant(X5.dropna())
-    model5 = sm.OLS(Y5.dropna(), X5).fit()
+    combined_data = pd.concat([Y5, X5], axis=1).dropna()
+    Y5_clean = combined_data['TC']
+    X5_clean = sm.add_constant(combined_data.drop(columns='TC'))
+
+    model5 = sm.OLS(Y5_clean, X5_clean).fit()
 
     # Équation 6: Chom
     Y6 = data['Chom']
     X6 = data[['Pibmond']]
     X6['Chom_lag'] = data['Chom'].shift(1)
-    X6['Pib_lag'] = data['Pib'].shift(1)
-    X6 = sm.add_constant(X6.dropna())
-    model6 = sm.OLS(Y6.dropna(), X6).fit()
+    combined_data = pd.concat([Y6, X6], axis=1).dropna()
+    Y6_clean = combined_data['Chom']
+    X6_clean = sm.add_constant(combined_data.drop(columns='Chom'))
+
+    model6 = sm.OLS(Y6_clean, X6_clean).fit()
 
     return model1, model2, model3, model4, model5, model6
 
@@ -77,17 +96,35 @@ st.title("Modèle à Équations Simultanées")
 # Affichage des équations
 st.subheader("Équations à Modéliser")
 st.markdown("""
-1. **PIB**: \( PIB_t = a_0 + a_1 PIB_{t-1} + b_1 FBCF_{t-1} + c_1 FBCF_t + d_1 G_t + e_1 G_{t-1} + f_1 X_t + g_1 X_{t-1} + h_1 M_t + i_1 M_{t-1} \)
+1. **PIB**: 
+   \[
+   PIB_t = a_0 + a_1 PIB_{t-1} + b_1 FBCF_{t-1} + c_1 FBCF_t + d_1 G_t + e_1 G_{t-1} + f_1 X_t + g_1 X_{t-1} + h_1 M_t + i_1 M_{t-1}
+   \]
 
-2. **DCF**: \( DCF_t = a_2 DCF_{t-1} + b_2 PIB_{t-1} + c_2 Taux\_interet_{t-1} \)
+2. **DCF**: 
+   \[
+   DCF_t = a_2 DCF_{t-1} + b_2 PIB_{t-1} + c_2 Taux\_interet_{t-1}
+   \]
 
-3. **FBCF**: \( FBCF_t = a_3 FBCF_{t-1} + b_3 Taux\_interet_t + c_3 PIB_{t-1} + d_3 Taux\_interet_{t-1} + e_3 Taux\_interet_t \)
+3. **FBCF**: 
+   \[
+   FBCF_t = a_3 FBCF_{t-1} + b_3 Taux\_interet_t + c_3 PIB_{t-1} + d_3 Taux\_interet_{t-1} + e_3 Taux\_interet_t
+   \]
 
-4. **MM**: \( MM_t = a_0^4 + a_4 MM_{t-1} + b_4 PIB_t + c_4 PIB_{t-1} + d_4 Taux\_interet_t + e_4 Taux\_interet_{t-1} + f_4 Infflation_{t-1} \)
+4. **MM**: 
+   \[
+   MM_t = a_0^4 + a_4 MM_{t-1} + b_4 PIB_t + c_4 PIB_{t-1} + d_4 Taux\_interet_t + e_4 Taux\_interet_{t-1} + f_4 Infflation_{t-1}
+   \]
 
-5. **TC**: \( TC_t = a_0^5 + a_5 PIB_t + b_5 PIB_{t-1} + c_5 Pibmond_t + d_5 TC_{t-1} \)
+5. **TC**: 
+   \[
+   TC_t = a_0^5 + a_5 PIB_t + b_5 PIB_{t-1} + c_5 Pibmond_t + d_5 TC_{t-1}
+   \]
 
-6. **Chom**: \( Chom_t = a_6 Chom_{t-1} + b_6 PIB_{t-1} + c_6 Pibmond_{t-1} \)
+6. **Chom**: 
+   \[
+   Chom_t = a_6 Chom_{t-1} + b_6 PIB_{t-1} + c_6 Pibmond_{t-1}
+   \]
 """)
 
 # Estimation des modèles
@@ -97,4 +134,4 @@ models = model_equations(data)
 st.subheader("Résultats des Estimations par Double Moindres Carrés")
 for i, model in enumerate(models, start=1):
     st.markdown(f"### Modèle {i}")
-    st.write(model.summary())  # Utiliser summary() au lieu de summary_df()
+    st.write(model.summary())
