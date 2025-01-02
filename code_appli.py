950759d955d1273774d4d446752fcb13537c3a2a
cezangue <cezangue@gmail.com>
2025-01-02 09:30:41 +0100

    Update code_appli.py

--- code_appli.py
+++ code_appli.py
@@ -2,7 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
-
+st.title("Mon Application Streamlit de modele à equations simultanées")
+st.subheader("Bienvenue l'application de simulation")
+st.write("Voici un exemple de texte pour expliquer ce que fait l'application: cette application vous presente l'évolution des grandes grandeurs macroéconomiques de la RCA; propose quelques simulations et un rapport d'analyse.")
 @st.cache_data()
 def get_data():
     df = pd.DataFrame(
@@ -32,3 +34,18 @@ else:
 st.markdown("""
 Vous pouvez éditer les valeurs dans le tableau ci-dessus. Les modifications seront affichées ci-dessous.
 """)
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Données pour le graphique
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Créer le graphique
+plt.plot(x, y)
+plt.title("Graphique Sinusoidal")
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+
+# Afficher le graphique dans Streamlit
+st.pyplot(plt)
